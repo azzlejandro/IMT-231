825301008c84fbceafb3576c2a349e72e7769282
azzlejandro <azzlejandro@gmail.com>
2024-09-19 09:09:32 -0400

    segundoCommit2

--- C++/juegoadivinanza.cpp
+++ C++/juegoadivinanza.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <stdio.h>
 
+using namespace std;
 
 int main(){
 
-std::cout<<"**************************************"<<std::endl;
+cout<<"**************************************"<<endl;
 
-std::cout<<"*Bienvenido al JUEGO de la adivinanza!*"<<std::endl;
-std::cout<<"***************************************"<<std::endl;
+cout<<"*Bienvenido al JUEGO de la adivinanza!*"<<endl;
+cout<<"***************************************"<<endl;
 
 }
 
